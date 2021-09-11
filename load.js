a20eee9883222e94e94a610b0575ed51fafd0c87
Man4070 <88619918+Man4070@users.noreply.github.com>
2021-09-11 23:57:33 +0600

    1.0.7

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-QuintOS.level = 4; // type of computer
-QuintOS.dir = 'GAMES'; // . means the same directory as the QuintOS.html file
-// QuintOS.gameSelect = 'pickAPath'; // name of the game file
-QuintOS.username = 'Man4070';
+QuintOS.level = 2; // type of computer
+QuintOS.dir = "GAMES"; // . means the same directory as the QuintOS.html file
+QuintOS.gameSelect = "wheelOfFortune"; // name of the game file
+QuintOS.username = "Man4070";
