fec47578cb47fdb4bf70e9a0f8a8b6d3f91c52cf
Man4070 <88619918+Man4070@users.noreply.github.com>
2021-12-07 21:28:29 +0600

    1.0.7

--- GAMES/SpeakAndSpell/speakAndSpell.js
+++ GAMES/SpeakAndSpell/speakAndSpell.js
@@ -1,9 +1,18 @@
 let inp;
 let word;
 
+let attempt = 0;
+let wordAmount = 0;
+let correctWords = 0;
+
 // value is the text the user entered in the input
 async function onSubmit(value) {
   if (value == word) {
+    if (wordAmount == 10) {
+      await play(speechSounds.you_are_correct);
+      endGame();
+      return;
+    }
     let phrases = [
       "that_is_correct_now_spell",
       "that_is_right_now_try",
@@ -13,14 +22,25 @@ async function onSubmit(value) {
     let rand = Math.random() * phrases.length;
     rand = Math.floor(rand);
     await play(speechSounds[phrases[rand]]);
+    correctWords++;
     nextWord();
+  } else if (attempt == 0) {
+    attempt++;
+    await play(speechSounds.wrong_try_again);
+    await play(wordSounds[word]);
   } else {
     await play(speechSounds.that_is_incorrect_the_correct_spelling_of);
     await play(wordSounds[word]);
     await play(speechSounds.is);
+    await erase(); // erase the screen
+    await text("*" + word);
     for (let i = 0; i < word.length; i++) {
       let letter = word[i];
       await play(letterSounds[letter]);
+    }
+    if (wordAmount == 10) {
+      endGame();
+      return;
     }
     await play(speechSounds.now_spell);
     nextWord();
@@ -33,8 +53,33 @@ function onChange(value) {
   letterSounds[letter].play();
 }
 
-async function nextWord() {
+async function endGame() {
   await erase(); // erase the screen
+  await text("Your score: " + correctWords);
+  if (correctWords >= 5) {
+    await play(speechSounds.you_win);
+  }
+  await play(speechSounds.here_is_your_score);
+  let nums = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+  ];
+
+  await play(wordSounds[nums[correctWords - 1]]);
+}
+
+async function nextWord() {
+  wordAmount++;
+  await erase(); // erase the screen
+  attempt = 0;
   let rand = Math.random() * words.length;
   rand = Math.floor(rand);
   word = words[rand];
@@ -46,9 +91,20 @@ async function nextWord() {
 }
 
 async function startGame() {
-  await alert("Press enter to start");
   await play(speechSounds.spell);
   nextWord();
 }
 
-startGame();
+async function scrollPrompt(msg) {
+  let modeInput = input();
+  // let user read the beginning
+  for (let i = 0; i < msg.length; i++) {
+    await text(msg.substring(i, i + 23));
+    if (i == 0) {
+      await delay(2000);
+    }
+    await delay(250);
+  }
+}
+
+scrollPrompt("Select mode A for short words or mode B for long words.");
--- load.js
+++ load.js
@@ -1,3 +1,3 @@
 QuintOS.username = "Man4070";
 QuintOS.dir = "GAMES";
-QuintOS.gameTitle = "SpeakAndSpell"; // name of the game file
+QuintOS.gameTitle = "Snake"; // name of the game file
