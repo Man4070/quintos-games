f01db943a9fa372e16473d4145a0d6feb9df07c8
Man4070 <88619918+Man4070@users.noreply.github.com>
2021-12-16 17:32:26 +0200

    1.0.8

--- GAMES/Snake/snake-preload.js
+++ GAMES/Snake/snake-preload.js
@@ -17,4 +17,25 @@ for (let i = 1; i < 10; i++) {
 let world = createTiles(8, 0, 16);
 world.spriteSheet = loadImage(QuintOS.dir + "/img/world.png");
 
-world.loadAni("egg", { pos: [1, 0] });
+for (let i = 0; i < 10; i++) {
+  world.loadAni("grass" + i, { pos: [1, i] });
+}
+
+let pipes = world.createGroup("pipes");
+pipes.loadAni("pipe-horiz", { pos: [2, 0] });
+pipes.loadAni("pipe-topRight", { pos: [2, 1] });
+pipes.loadAni("pipe-bottomLeft", { pos: [2, 2] });
+pipes.loadAni("pipe-start", { pos: [2, 3] });
+pipes.loadAni("pipe-middle", { pos: [2, 4] });
+pipes.loadAni("pipe-end", { pos: [2, 5] });
+
+world.loadAni("egg", { pos: [0, 0] });
+
+let snake = world.createSprite(10, 3, 2);
+snake.spriteSheet = loadImage(QuintOS.dir + "/img/snakes.png");
+snake.loadAni("head-up", { pos: [0, 0] });
+snake.loadAni("blink-up", { pos: [0, 1] });
+snake.loadAni("head-left", { pos: [0, 6] });
+snake.loadAni("blink-left", { pos: [0, 7] });
+snake.loadAni("eat-up", { pos: [1, 0], frames: 5 });
+snake.loadAni("eat-left", { pos: [1, 5], frames: 5 });
--- GAMES/Snake/snake.js
+++ GAMES/Snake/snake.js
@@ -7,8 +7,106 @@ let speed = 1; // snake speed
 text("SCORE: " + score, 17, 0);
 text("SPEED: " + score, 17, 11);
 
-world.createSprite("egg", 0, 0);
+let egg = world.createSprite("egg", 5, 10, 1);
+
+for (let i = 0; i < 15; i++) {
+  for (let j = 0; j < 20; j++) {
+    let rand = Math.floor(Math.random() * 9);
+    world.createSprite("grass" + rand, i, j);
+  }
+}
+
+for (let i = 0; i < 18; i++) {
+  let pipeType = "pipe-middle";
+  if (i == 0) {
+    pipeType = "pipe-start";
+  } else if (i == 17) {
+    pipeType = "pipe-end";
+  }
+  pipes.createSprite(pipeType, 0, i + 1, 1);
+  pipes.createSprite(pipeType, 14, i + 1, 1);
+}
+
+for (let i = 0; i < 13; i++) {
+  let pipeType = "pipe-middle";
+  if (i == 0) {
+    pipeType = "pipe-start";
+  } else if (i == 12) {
+    pipeType = "pipe-end";
+  }
+  let pipe = pipes.createSprite(pipeType, i + 1, 1, 1);
+  pipe.rotation = 90;
+  let pipe2 = pipes.createSprite(pipeType, i + 1, 20, 1);
+  pipe2.rotation = 90;
+}
+
+pipes.createSprite("pipe-topRight", 0, 19, 1);
+pipes.createSprite("pipe-topRight", 0, 0, 1).mirrorX(-1);
+pipes.createSprite("pipe-bottomLeft", 14, 0, 1);
+pipes.createSprite("pipe-bottomLeft", 14, 19, 1).mirrorX(-1);
+
+let direction = "up";
+
+function changeHeadAni(type) {
+  if (direction == "up") {
+    snake.ani(type + "-up");
+    snake.mirrorX(1);
+    snake.mirrorY(1);
+  } else if (direction == "down") {
+    snake.ani(type + "-up");
+    snake.mirrorX(1);
+    snake.mirrorY(-1);
+  } else if (direction == "left") {
+    snake.ani(type + "-left");
+    snake.mirrorX(1);
+    snake.mirrorY(1);
+  } else {
+    snake.ani(type + "-left");
+    snake.mirrorX(-1);
+    snake.mirrorY(1);
+  }
+}
+
+async function moveSnake() {
+  // move the snake
+  let type = "head";
+  if (
+    snake.row >= egg.row - 2 &&
+    snake.row <= egg.row + 2 &&
+    snake.col >= egg.col - 2 &&
+    snake.col <= egg.col + 2
+  ) {
+    type = "eat";
+  }
+  if (snake.row == egg.row && snake.col == egg.col) {
+    egg.row = Math.floor(Math.random() * 13 + 1);
+    egg.col = Math.floor(Math.random() * 18 + 1);
+  }
+  changeHeadAni(type);
+  await snake.move(direction, 0.5);
+  moveSnake();
+}
+
+moveSnake();
 
 function draw() {
-  // move the snake
+  background(colorPal(2));
+
+  snake.collide(pipes);
+
+  drawSprites();
+}
+
+function keyPressed() {
+  if (key == "ArrowUp" && direction != "down") {
+    direction = "up";
+  } else if (key == "ArrowDown" && direction != "up") {
+    direction = "down";
+  } else if (key == "ArrowLeft" && direction != "right") {
+    direction = "left";
+  } else if (key == "ArrowRight" && direction != "left") {
+    direction = "right";
+  }
+
+  log(direction);
 }
